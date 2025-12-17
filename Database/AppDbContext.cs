6caaa2a77b74f89b9358658fb0eeb7d86df4b73f
kemish bajgai <kemishbajgai4@gmail.com>
2025-12-17 14:49:25 +0545

    Initial commit: MVC setup, models, and EF Core migrations.

---
+++ Database/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DatasInformation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatasInformation.Database
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        
+        }
+       public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
