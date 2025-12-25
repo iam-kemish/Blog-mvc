44fc2f35b7251420532bb0dfcc2e45e259c703b8
iam-kemish <121624877+iam-kemish@users.noreply.github.com>
2025-12-25 17:38:43 +0545

    Added repositary logic for post model(not tested) (#2)

---
+++ Views/ViewModels/PostVM.cs
@@ -0,0 +1,12 @@
+﻿using DatasInformation.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DatasInformation.Views.ViewModels
+{
+    public class PostVM
+    {
+        public Post Post { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IFormFile FeatureImage { get; set; }
+    }
+}
