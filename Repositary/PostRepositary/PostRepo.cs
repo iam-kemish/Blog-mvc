6b585804230e847a7f4056af01dfa697085684c9
kemish bajgai <kemishbajgai4@gmail.com>
2025-12-25 17:32:02 +0545

    Added repositary logic for post model(not tested)

---
+++ Repositary/PostRepositary/PostRepo.cs
@@ -0,0 +1,60 @@
+﻿using DatasInformation.Database;
+using DatasInformation.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace DatasInformation.Repositary.PostRepositary
+{
+
+    public class PostRepo : IPost
+    {
+        private readonly AppDbContext _context;
+
+        public PostRepo(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public async Task AddAsync(Post post)
+        {
+            _context.Posts.Add(post);
+          await  _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+           var post = await _context.Posts.FirstOrDefaultAsync(u=>u.Id == id);
+            if (post != null) {
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Post>> GetAllAsync()
+        {
+        return  await  _context.Posts.Include(p=>p.Comments).ToListAsync();
+        }
+
+        public async Task<Post?> GetByIdAsync(int id)
+        {
+            var post = await _context.Posts.Include(p=>p.Comments).FirstOrDefaultAsync(u => u.Id == id);
+            return post;
+        }
+
+        public async Task UpdateAsync(Post post)
+        {
+           var existingPost = await _context.Posts.Include(p=>p.Comments).FirstOrDefaultAsync(p=>p.Id ==  post.Id);
+            if (existingPost != null) {
+                return;
+            } 
+            existingPost.Title = post.Title;
+            existingPost.Content = post.Content;
+            existingPost.Author = post.Author;
+            existingPost.CategoryId = post.CategoryId;
+            existingPost.FeatureImagePath = post.FeatureImagePath;
+
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
+ 
